1809b0d66beb1adf09e409467586479b41c114d5
ishwaryasaran07 <Pizza@1234567>
2022-07-20 11:56:52 +0100

    Intial Commit

---
+++ app/src/main/java/com/is6144/logistik/ProfileActivity.java
@@ -0,0 +1,83 @@
+package com.is6144.logistik;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
+    private String userID;
+
+
+    private Button logout;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = (Button) findViewById(R.id.signout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+            }
+        });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
+        final TextView fullNameTextView = (TextView) findViewById(R.id.fullName);
+        final TextView emailTextView = (TextView) findViewById(R.id.emailAddress);
+        final TextView registrationNumberTextView = (TextView) findViewById(R.id.registrationNumber);
+
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                if(userProfile != null){
+                    String fullName = userProfile.fullName;
+                    String email = userProfile.email;
+                    String registrationNumber = userProfile.driverRegistrationNumber;
+
+                    greetingTextView.setText("Welcome, " + fullName + "!");
+                    fullNameTextView.setText(fullName);
+                    emailTextView.setText(email);
+                    registrationNumberTextView.setText(registrationNumber);
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Something happened", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/is6144/logistik/User.java
@@ -0,0 +1,17 @@
+package com.is6144.logistik;
+
+public class User {
+
+    public String fullName, driverRegistrationNumber, email;
+
+    public User(){
+
+
+    }
+
+    public User(String fullName, String driverRegistrationNumber, String email){
+        this.fullName = fullName;
+        this.driverRegistrationNumber = driverRegistrationNumber;
+        this.email = email;
+    }
+}
